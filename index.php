d33bf1a6265054cbce8c71ad120a8c4e9847dae7
John <johnmedgar.2nd@gmail.com>
2025-08-12 21:01:48 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <title>Phone Web Utils</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Welcome to Phone Web Utils</h1>
+    <p>This website is for evil >:3</p>
+</body>
+</html>
